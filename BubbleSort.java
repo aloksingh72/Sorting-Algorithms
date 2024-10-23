3307f2318a3716d5aa0e1c3cc313df57abdd6d18
Alok Singh <aloksingh36757@gmail.com>
2024-10-23 19:26:04 +0530

    sorting updated

---
+++ BubbleSort.java
@@ -0,0 +1,35 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] a= {12,15,90,10,65,31,99,2};
+        //metho calling
+        bubbleSort(a);
+        for(int p:a){
+            System.out.print(p+" ");
+        }
+
+
+    }
+    public static void bubbleSort(int[] a) {
+        int n = a.length;
+        for(int i =0;i<n-1;i++){
+            int x =0;
+            for(int j=0;j<n-1-i;j++){
+                if(a[j]> a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    x++;
+                }
+
+            }
+            if(x==0){
+                break;
+            }
+
+        }
+
+
+
+    }
+    
+}
